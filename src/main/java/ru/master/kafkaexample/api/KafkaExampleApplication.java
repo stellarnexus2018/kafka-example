5be1ca97ae9768f01c1bccb9c5f4545fc140a5dc
igoryasha <stellarnexus@gmail.com>
2022-11-29 23:59:03 +0300

    init

---
+++ src/main/java/ru/master/kafkaexample/api/KafkaExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.master.kafkaexample.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/master/kafkaexample/api/KafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.master.kafkaexample.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
